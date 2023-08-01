b1fd5bbd9b676233fd6c07447bbf7723303ca332
david vidal <davidvidal44@gmail.com>
2023-07-31 20:44:31 -0700

    js file update

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var character = document.getElementById("character");
+var interval; 
 
-function moveleft(){
+function moveLeft(){
     var left= 
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character .style. left= left + 2+ "px";
+    character .style.left = left + 2+ "px";
 }
     
     function moveRight(){
@@ -14,12 +15,18 @@ function moveleft(){
 
         document.addEventListener ("keydown", event => {
             if (event.key==="ArrowLeft"){
+                interval=setInterval(moveLeft,1);
 
              }
 
              if (event.key==="ArrowRight"){
+                interval=setInterval(moveRight,1);
 
              }
             
         }
+    });
+document.addEventListener("keyup",event=>{
+    clearInterval(interval);
 
+})
